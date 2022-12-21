f586c019d4b3bf7bd577f2175f5cb02fd6b7a017
Tomek28611 <tomek286@yahoo.com>
2022-12-21 12:53:33 +0100

    zego connected

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/room/:roomId" element={<Room />} />
+          <Route path="/room/:roomID" element={<Room />} />
 
         </Routes>
     </div>
--- src/Room.js
+++ src/Room.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import React from 'react';
+import { useParams } from "react-router-dom";
+
+
 
 const Room = () => {
+    const { roomID } = useParams();
+  
+    const meeting = async (element) => {
+        const appID = 683252004;
+        const serverSecret = "8b9d00206e541ff0a288c98d298707bd";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().
+        toString(), "tomek286");
+
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+        zp.joinRoom({
+            container: element,
+            scenario: {
+                mode: ZegoUIKitPrebuilt.GroupCall,
+            },
+        });
+    };
+
   return (
-    <div>Room</div>
+    <div ref={meeting} style={{ width: "100vw", height: "100vw"}}>
+
+    </div>
   )
 }
 
